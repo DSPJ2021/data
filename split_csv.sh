15e1d0d35b9a01685fbb149ea67f2612e31b83e5
Kevin Kreutz <kreutz@campus.tu-berlin.de>
2021-07-05 21:04:06 +0200

    July-2017 dataset, split_csv to split by sensor_id

---
+++ split_csv.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# $1 -> .csv
+#splits csv by seconds column into multiple files $1.csv
+
+CSV=$1
+
+# delete first column (id), sort by 2nd
+awk -F',' '{print $2,$3,$4,$5,$6,$7,$8}' $1 | sort -u > data.csv #to import more dates: > -> >>
+
+#
+awk -F" " '{ print > $1 ".csv" }' data.csv
+
+rm $1 data_july.csv
+mv sensor_id.csv legende.txt
+
+
+#for i in "${!CSV_list[@]}"; do
+#	sed -i 1i",sensor_id,location,lat,lon,timestamp,pressure,temperature,humidity" ${CSV_list[i]}
+#done
